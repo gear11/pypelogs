c827ab45547973918d3bb2cdcb766cce7bc2fd15
Andy Jenkins <andy@gear11.com>
2014-08-24 21:12:24 -0400

    For CSV, add delimiter options and specifying headers.

--- pypein/csv_in.py
+++ pypein/csv_in.py
@@ -3,6 +3,9 @@ import logging
 import csv
 LOG = logging.getLogger("csv_in")
 
+
+DELIMS = {"tab": "\t", "comma": ","}
+
 class CSVIn(object):
     def __init__(self, spec=None):
         args = spec.split(",", 1)
@@ -10,12 +13,21 @@ class CSVIn(object):
             self.opts = utils.to_dict(args[1])
         else:
             self.opts = {}
-        self.headers = None
-        self.fo = utils.fopen(args[0], self.opts.get('enc', 'utf-8'))
+        if "headers" in self.opts:
+            self.headers = self.opts['headers'].split(':')
+        else:
+            self.headers = None
+        if "delim" in self.opts:
+            dkey = self.opts["delim"]
+            delim = DELIMS[dkey] if dkey in DELIMS else dkey
+        else:
+            delim = ","
+        fo = utils.fopen(args[0], self.opts.get('enc', 'utf-8'))
+        self.reader = csv.reader(fo, delimiter=delim)
 
     def __iter__(self):
         count = 0
-        for vals in csv.reader(self.fo):
+        for vals in self.reader:
             count += 1
             if not self.headers:
                 self.headers = vals
