73ae3566ab617fa59a1dfc1d4924354d85be8da3
Andy Jenkins <andy@gear11.com>
2014-08-24 21:14:12 -0400

    For sort, allow descending.

--- pypef/sort.py
+++ pypef/sort.py
@@ -8,12 +8,18 @@ class Sort(filter.Filter):
     """Sorts on the indicated, comma-delimited list of fields"""
     def __init__(self, spec):
         super(filter.Filter, self).__init__()
-        self.sort_keys = spec.split(',')
+        self.sort_keys = []
+        self.desc = set()
+        for field in spec.split(','):
+            if '=' in field:
+                field, desc = field.split('=', 1)
+                if desc.lower() == 'desc':
+                    self.desc.add(field)
+            self.sort_keys.append(field)
 
     def filter_events(self, events):
         l = list(events)
-        s = l
-        for k in reversed(self.sort_keys):
-            s = sorted(s, key=operator.itemgetter(k))
-        for e in s:
+        for field in reversed(self.sort_keys):
+            l = sorted(l, key=operator.itemgetter(field), reverse=(field in self.desc))
+        for e in l:
             yield e
